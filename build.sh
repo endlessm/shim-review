4a69ea8c11c6630a45700d3c880796a48a23fbf3
Dan Nicholson <dbn@endlessos.org>
2024-09-12 14:57:39 -0600

    Adjust build script for gbp export directory
    Now the build is happening in the /shim-build directory in the
    container, so just copy the whole thing out for analysis.

--- build.sh
+++ build.sh
@@ -8,10 +8,8 @@ builddir="$srcdir/build"
 $DOCKER build --no-cache -t endless-shim-review .
 
 rm -rf "$builddir"
-mkdir -p "$builddir"
 id=$($DOCKER create endless-shim-review)
-$DOCKER cp "$id:/shim/shimx64.efi" "$builddir/"
-$DOCKER cp "$id:/shim/shimx64.efi.hd" "$builddir/"
+$DOCKER cp "$id:/shim-build/" "$builddir"
 $DOCKER rm -v "$id"
 
 echo "Review shim: $srcdir/shimx64.efi"
